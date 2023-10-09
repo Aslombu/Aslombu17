468cdbcec94101c9c9603ca50c5bc5d30b79be92
Aslombu <aslombu1117@gmail.com>
2023-10-09 14:59:13 +0500

    githubdagilarga salom

---
+++ salomlashish/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hammaga salom qalysilar");
+        System.out.println("hammaga salom qalysilar");
+        System.out.println("hammaga salom qalysilar");
+        System.out.println("hammaga salom qalysilar");
+        System.out.println("hammaga salom qalysilar");
+    }
+}
